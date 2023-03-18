754faf5a9482f054b113d26333955319cd490b0f
Aayush Karki <akarki@ramapo.edu>
2023-03-17 20:15:58 -0400

    creates expense filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
 import { useState } from "react";
+import ExpenseFilter from "./components/ExpenseFilter";
 import Form from "./components/ExpenseForm";
 import ExpenseList from "./components/ExpenseList";
 
 function App() {
 	const categoryList = ["Groceries", "Utilities", "Entertainment"];
 
+	const [selectedCategory, setSelectedCategory] = useState("");
+
 	const [expenses, setExpenses] = useState([
 		{ id: 1, description: "aaa", amount: 10, category: "Utilities" },
 		{ id: 2, description: "bbb", amount: 10, category: "Utilities" },
 		{ id: 3, description: "ccc", amount: 10, category: "Utilities" },
 		{ id: 4, description: "ddd", amount: 10, category: "Utilities" },
+		{ id: 4, description: "ddd", amount: 10, category: "Groceries" },
 	]);
+
+	const visibleExpenses = selectedCategory
+		? expenses.filter((expense) => expense.category === selectedCategory)
+		: expenses;
 
 	return (
 		<div>
-			<Form categorys={categoryList}></Form>
+			<div className="mb-3">
+				<Form categorys={categoryList}></Form>
+			</div>
+			<div className="mb-3">
+				<ExpenseFilter
+					onSelectCategory={(category) =>
+						setSelectedCategory(category)
+					}
+				></ExpenseFilter>
+			</div>
 			<ExpenseList
-				expense={expenses}
+				expense={visibleExpenses}
 				onDelete={(id) =>
 					setExpenses(expenses.filter((expense) => expense.id !== id))
 				}
